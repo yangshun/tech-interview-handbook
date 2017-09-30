0f78cf3657834b468857d51f20b4f6c2b3b94b22
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-09-30 21:55:53 +0800

    Use const

--- utilities/javascript/binarySearch.js
+++ utilities/javascript/binarySearch.js
@@ -2,7 +2,7 @@ function binarySearch(arr, target) {
     let left = 0;
     let right = arr.length - 1;
     while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
+        const mid = left + Math.floor((right - left) / 2);
         if (arr[mid] === target) {
             return mid;
         }
