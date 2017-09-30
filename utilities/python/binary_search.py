4fa28c04ee39be919cf4faf4715aa490b1f28c72
Austin Green <austinzgreen@gmail.com>
2017-09-30 09:51:37 -0400

    Return -1 if target is not in array (#9)
    * return -1 if target is not in array
    
    * return -1 if target is not in array (python)
    
    * <= in binary search while loop

--- utilities/python/binary_search.py
+++ utilities/python/binary_search.py
@@ -1,7 +1,7 @@
 def binary_search(arr, target):
     left = 0;
     right = len(arr) - 1
-    while left < right:
+    while left <= right:
         mid = left + (right - left) // 2;
         if arr[mid] == target:
             return mid
@@ -9,14 +9,14 @@ def binary_search(arr, target):
             left = mid + 1
         else:
             right = mid - 1
-    return left
+    return -1
 
 print(binary_search([1, 2, 3, 10], 1) == 0)
 print(binary_search([1, 2, 3, 10], 2) == 1)
 print(binary_search([1, 2, 3, 10], 3) == 2)
 print(binary_search([1, 2, 3, 10], 10) == 3)
-print(binary_search([1, 2, 3, 10], 9) == 3)
-print(binary_search([1, 2, 3, 10], 4) == 3)
-print(binary_search([1, 2, 3, 10], 0) == 0)
-print(binary_search([1, 2, 3, 10], 11) == 3)
-print(binary_search([5, 7, 8, 10], 3) == 0)
+print(binary_search([1, 2, 3, 10], 9) == -1)
+print(binary_search([1, 2, 3, 10], 4) == -1)
+print(binary_search([1, 2, 3, 10], 0) == -1)
+print(binary_search([1, 2, 3, 10], 11) == -1)
+print(binary_search([5, 7, 8, 10], 3) == -1)
