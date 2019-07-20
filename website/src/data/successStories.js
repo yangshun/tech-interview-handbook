144db8f9097b48d60e593acc573e85309c155114
Yangshun Tay <tay.yang.shun@gmail.com>
2019-07-20 03:19:59 -0700

    website: add Erin success story

--- website/src/data/successStories.js
+++ website/src/data/successStories.js
@@ -2,6 +2,28 @@ import React from 'react';
 
 export default [
   {
+    name: 'Erin Teo',
+    title: 'Front End Engineer, Facebook',
+    thumbnail: 'https://avatars1.githubusercontent.com/u/5081708?s=460&v=4',
+    quote: (
+      <>
+        Preparing for my first rounds of tech interviews was really daunting - I
+        wasn't sure what to expect and where to start. This handbook together
+        with the{' '}
+        <a
+          href="https://github.com/yangshun/front-end-interview-handbook"
+          target="_blank"
+          rel="noreferrer noopener">
+          Front End Interview Handbook
+        </a>{' '}
+        was a great starting point for me. It clearly describes each part of the
+        process and has tons of awesome tips and resources. With this handbook
+        and lots of practice, I managed to get offers from Facebook, Dropbox and
+        Amazon!
+      </>
+    ),
+  },
+  {
     name: 'Siddhesh Karekar',
     title: 'Software Engineer, Google',
     thumbnail: 'https://avatars1.githubusercontent.com/u/18223888?s=460&v=4',
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -44,83 +44,89 @@ function Home() {
         </div>
       </header>
       <main>
-        <div className="container margin-vert--xl">
-          <div className="row">
-            <div className="col col--10 col--offset-1">
-              <h2 className="text--center  margin-bottom--xl">
-                Why Tech Interview Handbook?
-              </h2>
-              <div className="row margin-vert--lg">
-                <div className="col">
-                  <h3>From Zero to Hero</h3>
-                  <p>
-                    Go from zero to tech interview hero with this handbook. No
-                    prior interview experience needed.
-                  </p>
+        <div className="padding-vert--xl">
+          <div className="container">
+            <div className="row">
+              <div className="col col--10 col--offset-1">
+                <h2 className="text--center  margin-bottom--xl">
+                  Why Tech Interview Handbook?
+                </h2>
+                <div className="row margin-vert--lg">
+                  <div className="col">
+                    <h3>From Zero to Hero</h3>
+                    <p>
+                      Go from zero to tech interview hero with this handbook. No
+                      prior interview experience needed.
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h3>Curated Practice Questions</h3>
+                    <p>
+                      No one has time to practice a few hundred Leetcode
+                      questions. We tell you which are the best questions to
+                      practice.
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h3>Interview Cheatsheet</h3>
+                    <p>
+                      Straight-to-the-point Do's and Don'ts during an interview
+                    </p>
+                  </div>
                 </div>
-                <div className="col">
-                  <h3>Curated Practice Questions</h3>
-                  <p>
-                    No one has time to practice a few hundred Leetcode
-                    questions. We tell you which are the best questions to
-                    practice.
-                  </p>
-                </div>
-                <div className="col">
-                  <h3>Interview Cheatsheet</h3>
-                  <p>
-                    Straight-to-the-point Do's and Don'ts during an interview
-                  </p>
-                </div>
-              </div>
-              <div className="row margin-vert--lg">
-                <div className="col">
-                  <h3>Practical Algorithm Tips</h3>
-                  <p>
-                    Practical tips for every algorithm topic - common techniques
-                    and corner cases to look out for.
-                  </p>
-                </div>
-                <div className="col">
-                  <h3>Behavioral Questions</h3>
-                  <p>
-                    Check out what behavioral questions companies commonly ask
-                    and you can prepare your answers ahead of time.
-                  </p>
-                </div>
-                <div className="col">
-                  <h3>Tested and Proven</h3>
-                  <p>
-                    Countless engineers have gotten their dream jobs with its
-                    help.
-                  </p>
+                <div className="row margin-vert--lg">
+                  <div className="col">
+                    <h3>Practical Algorithm Tips</h3>
+                    <p>
+                      Practical tips for every algorithm topic - common
+                      techniques and corner cases to look out for.
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h3>Behavioral Questions</h3>
+                    <p>
+                      Check out what behavioral questions companies commonly ask
+                      and you can prepare your answers ahead of time.
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h3>Tested and Proven</h3>
+                    <p>
+                      Countless engineers have gotten their dream jobs with its
+                      help.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="container margin-vert--xl">
-          <div className="row">
-            <div className="col col--6 col--offset-3">
-              <div className="margin-vert--lg text--center">
-                <h2>Success Stories</h2>
-              </div>
-              {successStories.map(user => (
-                <div className="card margin-vert--lg" key={user.name}>
-                  <div className="card__header">
-                    <div className="avatar">
-                      <img className="avatar__photo" src={user.thumbnail} />
-                      <div className="avatar__intro">
-                        <h4 className="avatar__name">{user.name}</h4>
-                        <small className="avatar__subtitle">{user.title}</small>
+        <div className={styles.sectionAlt}>
+          <div className="container padding-vert--xl">
+            <div className="row">
+              <div className="col col--6 col--offset-3">
+                <div className="margin-vert--lg text--center">
+                  <h2>Success Stories</h2>
+                </div>
+                {successStories.map(user => (
+                  <div className="card margin-vert--lg" key={user.name}>
+                    <div className="card__header">
+                      <div className="avatar">
+                        <img className="avatar__photo" src={user.thumbnail} />
+                        <div className="avatar__intro">
+                          <h4 className="avatar__name">{user.name}</h4>
+                          <small className="avatar__subtitle">
+                            {user.title}
+                          </small>
+                        </div>
                       </div>
                     </div>
+                    <div className="card__body">
+                      <blockquote>"{user.quote}"</blockquote>
+                    </div>
                   </div>
-                  <div className="card__body">
-                    <blockquote>"{user.quote}"</blockquote>
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
