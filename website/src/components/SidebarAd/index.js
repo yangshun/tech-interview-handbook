17b8fb970d00c579e29f1b0a42b36e41241b030c
Yangshun <tay.yang.shun@gmail.com>
2021-05-31 00:42:35 +0800

    website: track referral clicks via gtag

--- website/src/components/SidebarAd/index.js
+++ website/src/components/SidebarAd/index.js
@@ -13,7 +13,10 @@ function SidebarAd() {
       className={styles.container}
       href="https://www.moonchaser.io/?utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_docs_sidebar"
       target="_blank"
-      rel="noreferrer noopener">
+      rel="noreferrer noopener"
+      onClick={() => {
+        window.gtag('event', 'moonchaser.click');
+      }}>
       <p className={styles.tagline}>
         <strong>Get paid more.</strong> Receive risk-free salary negotiation
         help from Moonchaser. You pay nothing unless your offer is increased.
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -68,7 +68,10 @@ function Home() {
                       className="button button--secondary button--lg"
                       href="https://www.moonchaser.io/?utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_homepage"
                       rel="noreferrer noopener"
-                      target="_blank">
+                      target="_blank"
+                      onClick={() => {
+                        window.gtag('event', 'moonchaser.click');
+                      }}>
                       Find Out More&nbsp;&nbsp;→
                     </a>
                   </div>
