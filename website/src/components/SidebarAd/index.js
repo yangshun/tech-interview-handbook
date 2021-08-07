27894a7389a599abe40c2f8de0ee99c1f2d7446c
Yangshun <tay.yang.shun@gmail.com>
2021-08-07 23:03:41 +0800

    website: make ad component change background color

--- website/src/components/SidebarAd/index.js
+++ website/src/components/SidebarAd/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
+import clsx from 'clsx';
+
 import styles from './styles.module.css';
 
-export default function SidebarAd() {
+const BACKGROUNDS = [
+  styles.backgroundBlue,
+  styles.backgroundGreen,
+  styles.backgroundOrange,
+  styles.backgroundPurple,
+  styles.backgroundRed,
+];
+
+export default React.memo(function SidebarAd() {
+  const backgroundClass = BACKGROUNDS[Math.floor(Math.random() * BACKGROUNDS.length)];
+  
   return (
     <a
-      className={styles.container}
+      className={clsx(styles.container, backgroundClass)}
       href="https://www.moonchaser.io/?utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_docs_sidebar"
       target="_blank"
       rel="noreferrer noopener"
@@ -20,4 +32,4 @@ export default function SidebarAd() {
       </p>
     </a>
   );
-}
+});
