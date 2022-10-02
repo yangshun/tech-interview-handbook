5734758f96cbbcc06ccf30e4faee8f34c8fec189
Paul Mureşan <17965058+paulmrsn@users.noreply.github.com>
2022-10-03 00:23:08 +0100

    contents: fix typo in choosing between companies (#302)
    Co-authored-by: Paul Muresan <mmuresanpaul@gmail.com>

--- apps/portal/src/components/resumes/ResumeReviewsTitle.tsx
+++ apps/portal/src/components/resumes/ResumeReviewsTitle.tsx
@@ -1,5 +1,3 @@
 export default function ResumeReviewsTitle() {
-  return (
-    <h1 className="text-center text-4xl font-bold">Resume Reviews {id}</h1>
-  );
+  return <h1 className="text-center text-4xl font-bold">Resume Reviews</h1>;
 }
