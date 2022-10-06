e8dad2d5f135ae22d4cb056c6c3b352a6aee9fcf
Keane Chan <e0425601@u.nus.edu>
2022-10-06 17:14:36 +0800

    [resumes][feat] submit resume mutation

--- apps/portal/src/server/router/index.ts
+++ apps/portal/src/server/router/index.ts
@@ -2,6 +2,7 @@ import superjson from 'superjson';
 
 import { createRouter } from './context';
 import { protectedExampleRouter } from './protected-example-router';
+import { resumesResumeUserRouter } from './resumes-resume-user-router';
 import { todosRouter } from './todos';
 import { todosUserRouter } from './todos-user-router';
 
@@ -12,7 +13,8 @@ export const appRouter = createRouter()
   // Example routers. Learn more about tRPC routers: https://trpc.io/docs/v9/router
   .merge('auth.', protectedExampleRouter)
   .merge('todos.', todosRouter)
-  .merge('todos.user.', todosUserRouter);
+  .merge('todos.user.', todosUserRouter)
+  .merge('resumes.resume.user.', resumesResumeUserRouter);
 
 // Export type definition of API
 export type AppRouter = typeof appRouter;
