e3b91088359318097ed8ae66fb4cbf2ec5cc671d
Tan Su Yin <e0425292@u.nus.edu>
2022-10-07 23:08:03 +0800

    [resumes][feat] Add router to fetch all resumes

---
+++ apps/portal/src/types/resume.d.ts
@@ -0,0 +1,14 @@
+export type Resume = {
+  additionalInfo: string?;
+  createdAt: Date;
+  experience: string;
+  id: string;
+  location: string;
+  numComments: number;
+  numStars: number;
+  role: string;
+  title: string;
+  updatedAt: Date;
+  url: string;
+  user: string;
+};
