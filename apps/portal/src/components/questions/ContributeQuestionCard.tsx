a0532d57f8fb3e4b039d1cb1152cbdb25c66c5a2
wlren <weilinwork99@gmail.com>
2022-10-07 20:40:49 +0800

    [questions][ui] Contribute questions dialog

--- apps/portal/src/components/questions/ContributeQuestionCard.tsx
+++ apps/portal/src/components/questions/ContributeQuestionCard.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps, ForwardedRef } from 'react';
+import { useState } from 'react';
 import { forwardRef } from 'react';
 import type { UseFormRegisterReturn } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
@@ -8,6 +9,8 @@ import {
   QuestionMarkCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button, TextInput } from '@tih/ui';
+
+import ContributeQuestionModal from './ContributeQuestionModal';
 
 export type ContributeQuestionData = {
   company: string;
@@ -45,44 +48,56 @@ export default function ContributeQuestionCard({
   onSubmit,
 }: ContributeQuestionCardProps) {
   const { register, handleSubmit } = useForm<ContributeQuestionData>();
+  const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
-    <form
-      className="flex flex-col items-stretch justify-center gap-2 rounded-md border border-slate-300 p-4"
-      onSubmit={handleSubmit(onSubmit)}>
-      <FormTextInput
-        isLabelHidden={true}
-        label="Question"
-        placeholder="Contribute a question"
-        {...register('questionContent')}
-      />
-      <div className="flex items-end justify-center gap-x-2">
-        <div className="min-w-[150px] flex-1">
-          <FormTextInput
-            label="Company"
-            startAddOn={BuildingOffice2Icon}
-            startAddOnType="icon"
-            {...register('company')}
+    <>
+      <form
+        className="flex flex-col items-stretch justify-center gap-2 rounded-md border border-slate-300 p-4"
+        onSubmit={handleSubmit(onSubmit)}>
+        <FormTextInput
+          isLabelHidden={true}
+          label="Question"
+          placeholder="Contribute a question"
+          {...register('questionContent')}
+        />
+        <div className="flex items-end justify-center gap-x-2">
+          <div className="min-w-[150px] flex-1">
+            <FormTextInput
+              label="Company"
+              startAddOn={BuildingOffice2Icon}
+              startAddOnType="icon"
+              {...register('company')}
+            />
+          </div>
+          <div className="min-w-[150px] flex-1">
+            <FormTextInput
+              label="Question type"
+              startAddOn={QuestionMarkCircleIcon}
+              startAddOnType="icon"
+              {...register('questionType')}
+            />
+          </div>
+          <div className="min-w-[150px] flex-1">
+            <FormTextInput
+              label="Date"
+              startAddOn={CalendarDaysIcon}
+              startAddOnType="icon"
+              {...register('date')}
+            />
+          </div>
+          <Button
+            label="Contribute"
+            type="submit"
+            variant="primary"
+            onClick={() => setOpen(true)}
           />
         </div>
-        <div className="min-w-[150px] flex-1">
-          <FormTextInput
-            label="Question type"
-            startAddOn={QuestionMarkCircleIcon}
-            startAddOnType="icon"
-            {...register('questionType')}
-          />
-        </div>
-        <div className="min-w-[150px] flex-1">
-          <FormTextInput
-            label="Date"
-            startAddOn={CalendarDaysIcon}
-            startAddOnType="icon"
-            {...register('date')}
-          />
-        </div>
-        <Button label="Contribute" type="submit" variant="primary" />
-      </div>
-    </form>
+      </form>
+
+      <ContributeQuestionModal
+        contributeState={isOpen}
+        setContributeState={setOpen}></ContributeQuestionModal>
+    </>
   );
 }
