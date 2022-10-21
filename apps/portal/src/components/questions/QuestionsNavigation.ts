16b6484d3404e6eafb579d7ee3d513e352c73f5d
Jeff Sieu <jeffsy00@gmail.com>
2022-10-21 21:12:52 +0800

    [question][ui] add Home to navigation

--- apps/portal/src/components/questions/QuestionsNavigation.ts
+++ apps/portal/src/components/questions/QuestionsNavigation.ts
@@ -1,6 +1,7 @@
 import type { ProductNavigationItems } from '~/components/global/ProductNavigation';
 
 const navigation: ProductNavigationItems = [
+  { href: '/questions', name: 'Home' },
   { href: '/questions/lists', name: 'My Lists' },
   { href: '/questions/my-questions', name: 'My Questions' },
   { href: '/questions/history', name: 'History' },
