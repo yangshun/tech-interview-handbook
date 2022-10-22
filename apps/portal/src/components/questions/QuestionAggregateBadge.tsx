d5f024f872fb7517a53cd2407fc8ddd19465e955
Jeff Sieu <jeffsy00@gmail.com>
2022-10-22 12:53:36 +0800

    [questions][feat] show question statistics dropdown

--- apps/portal/src/components/questions/QuestionAggregateBadge.tsx
+++ apps/portal/src/components/questions/QuestionAggregateBadge.tsx
@@ -1,7 +1,9 @@
 import type { ComponentProps } from 'react';
 import { useMemo } from 'react';
-import { Popover } from '@headlessui/react';
+import { usePopperTooltip } from 'react-popper-tooltip';
 import { Badge } from '@tih/ui';
+
+import 'react-popper-tooltip/dist/styles.css';
 
 type BadgeProps = ComponentProps<typeof Badge>;
 
@@ -13,6 +15,13 @@ export default function QuestionAggregateBadge({
   statistics,
   ...badgeProps
 }: QuestionAggregateBadgeProps) {
+  const { getTooltipProps, setTooltipRef, setTriggerRef, visible } =
+    usePopperTooltip({
+      interactive: true,
+      placement: 'bottom-start',
+      trigger: ['focus', 'hover'],
+    });
+
   const mostCommonStatistic = useMemo(
     () =>
       Object.entries(statistics).reduce(
@@ -27,6 +36,15 @@ export default function QuestionAggregateBadge({
     [statistics],
   );
 
+  const sortedStatistics = useMemo(
+    () =>
+      Object.entries(statistics)
+        .sort((a, b) => b[1] - a[1])
+        .map(([key, value]) => ({ key, value })),
+
+    [statistics],
+  );
+
   const additionalStatisticCount = Object.keys(statistics).length - 1;
 
   const label = useMemo(() => {
@@ -37,15 +55,26 @@ export default function QuestionAggregateBadge({
   }, [mostCommonStatistic, additionalStatisticCount]);
 
   return (
-    <Popover>
-      <Popover.Button>
+    <>
+      <button ref={setTriggerRef} className="rounded-full" type="button">
         <Badge label={label} {...badgeProps} />
-      </Popover.Button>
-      <Popover.Panel>
-        <div className="flex flex-col gap-2">
-          <h1>Hello</h1>
+      </button>
+      {visible && (
+        <div ref={setTooltipRef} {...getTooltipProps()}>
+          <div className="flex flex-col gap-2 rounded-md bg-white p-2 shadow-md">
+            <ul>
+              {sortedStatistics.map(({ key, value }) => (
+                <li
+                  key={key}
+                  className="flex justify-between gap-x-4 rtl:flex-row-reverse">
+                  <span className="flex text-start font-bold">{key}</span>
+                  <span className="float-end">{value}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </Popover.Panel>
-    </Popover>
+      )}
+    </>
   );
 }
