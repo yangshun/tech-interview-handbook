bb212bf6961412eee93ac08de60abbd7c5ffe254
Bryann Yeap Kok Keong <bryannyeapkk@gmail.com>
2022-10-24 16:14:35 +0800

    [offers][chore] Merge main into branch

--- apps/portal/src/pages/offers/test/getAnalysis.tsx
+++ apps/portal/src/pages/offers/test/getAnalysis.tsx
@@ -5,7 +5,7 @@ import { trpc } from '~/utils/trpc';
 function GetAnalysis() {
   const analysis = trpc.useQuery([
     'offers.analysis.get',
-    { profileId: 'cl9luzsqh0005utr2d7jpjabt' },
+    { profileId: 'cl9lwe9m902k5utskjs52wc0j' },
   ]);
 
   return <div>{JSON.stringify(analysis.data)}</div>;
