3c8029625355c29d090596a3e401e9cc2a8f545e
Stuart Long Chay Boon <chayboon@gmail.com>
2022-10-24 21:27:40 +0800

    [offers][refactor] refactor random name generator code

--- apps/portal/src/utils/offers/randomNameGenerator.ts
+++ apps/portal/src/utils/offers/randomNameGenerator.ts
@@ -1,5 +1,8 @@
 import type { Config } from 'unique-names-generator';
 import { adjectives, animals,colors, uniqueNamesGenerator } from 'unique-names-generator';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 const customConfig: Config = {
   dictionaries: [adjectives, colors, animals],
@@ -8,6 +11,23 @@ const customConfig: Config = {
 };
 
 
-export default function generateRandomName(): string {
-    return uniqueNamesGenerator(customConfig)
+export default async function generateRandomName(): Promise<string> {
+    let uniqueName: string = uniqueNamesGenerator(customConfig);
+
+    let sameNameProfiles = await prisma.offersProfile.findMany({
+        where: {
+            profileName: uniqueName
+        }
+    })
+
+    while (sameNameProfiles.length !== 0) {
+        uniqueName = uniqueNamesGenerator(customConfig);
+        sameNameProfiles = await prisma.offersProfile.findMany({
+            where: {
+            profileName: uniqueName
+            }
+        })
+    }
+
+    return uniqueName
 }
