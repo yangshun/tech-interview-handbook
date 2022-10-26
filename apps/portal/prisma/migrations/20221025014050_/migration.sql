77293f965f0540a41ff245e5d5c39729226e32f3
hpkoh <hpkoh99@gmail.com>
2022-10-26 18:09:14 +0800

    [questions][fix] remove erronous migration file

--- apps/portal/prisma/migrations/20221025014050_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "QuestionsQuestion_contentSearch_idx";
-
--- AlterTable
-ALTER TABLE "QuestionsQuestion" ALTER COLUMN "contentSearch" DROP DEFAULT;
-
--- CreateIndex
-CREATE INDEX "QuestionsQuestion_contentSearch_idx" ON "QuestionsQuestion"("contentSearch");
