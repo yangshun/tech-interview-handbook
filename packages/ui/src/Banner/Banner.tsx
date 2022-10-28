d8d4b0f09294e211f1d1ac74f2ed257048cd0d16
Yangshun Tay <tay.yang.shun@gmail.com>
2022-10-28 17:56:09 +0800

    [ui] standardize naming

--- packages/ui/src/Banner/Banner.tsx
+++ packages/ui/src/Banner/Banner.tsx
@@ -18,7 +18,7 @@ export default function Banner({ children, size = 'md', onHide }: Props) {
         size === 'sm' && 'text-sm',
         size === 'xs' && 'text-xs',
       )}>
-      <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-2 px-3 sm:px-6 lg:px-8">
         <div className="pr-16 sm:px-16 sm:text-center">
           <p className="font-medium text-white">{children}</p>
         </div>
