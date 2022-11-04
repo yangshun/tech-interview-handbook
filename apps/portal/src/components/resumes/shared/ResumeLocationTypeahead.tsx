6fc312022a847565da99c1937cc43c73a83c43d6
Keane Chan <e0425601@u.nus.edu>
2022-11-04 18:00:16 +0800

    [resumes][feat] add location and role typeaheads

---
+++ apps/portal/src/components/resumes/shared/ResumeLocationTypeahead.tsx
@@ -0,0 +1,57 @@
+import type { ComponentProps } from 'react';
+import { useState } from 'react';
+import type { TypeaheadOption } from '@tih/ui';
+import { Typeahead } from '@tih/ui';
+
+import { trpc } from '~/utils/trpc';
+
+type BaseProps = Pick<
+  ComponentProps<typeof Typeahead>,
+  | 'disabled'
+  | 'errorMessage'
+  | 'isLabelHidden'
+  | 'placeholder'
+  | 'required'
+  | 'textSize'
+>;
+
+type Props = BaseProps &
+  Readonly<{
+    onSelect: (option: TypeaheadOption | null) => void;
+    value?: TypeaheadOption | null;
+  }>;
+
+export default function ResumeLocationTypeahead({
+  onSelect,
+  value,
+  ...props
+}: Props) {
+  const [query, setQuery] = useState('');
+  const countries = trpc.useQuery([
+    'locations.countries.list',
+    {
+      name: query,
+    },
+  ]);
+
+  const { data } = countries;
+
+  return (
+    <Typeahead
+      label="Location"
+      noResultsMessage="No location found"
+      nullable={true}
+      options={
+        data?.map(({ id, name }) => ({
+          id,
+          label: name,
+          value: id,
+        })) ?? []
+      }
+      value={value}
+      onQueryChange={setQuery}
+      onSelect={onSelect}
+      {...props}
+    />
+  );
+}
---
+++ apps/portal/src/components/resumes/shared/ResumeRoleTypeahead.tsx
@@ -0,0 +1,53 @@
+import type { ComponentProps } from 'react';
+import { useState } from 'react';
+import type { TypeaheadOption } from '@tih/ui';
+import { Typeahead } from '@tih/ui';
+
+import { JobTitleLabels } from '../../shared/JobTitles';
+
+type BaseProps = Pick<
+  ComponentProps<typeof Typeahead>,
+  | 'disabled'
+  | 'errorMessage'
+  | 'isLabelHidden'
+  | 'placeholder'
+  | 'required'
+  | 'textSize'
+>;
+
+type Props = BaseProps &
+  Readonly<{
+    onSelect: (option: TypeaheadOption | null) => void;
+    value?: TypeaheadOption | null;
+  }>;
+
+export default function ResumeRoleTypeahead({
+  onSelect,
+  value,
+  ...props
+}: Props) {
+  const [query, setQuery] = useState('');
+  const options = Object.entries(JobTitleLabels)
+    .map(([slug, label]) => ({
+      id: slug,
+      label,
+      value: slug,
+    }))
+    .filter(
+      ({ label }) =>
+        label.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) > -1,
+    );
+
+  return (
+    <Typeahead
+      label="Roles"
+      noResultsMessage="No available roles."
+      nullable={true}
+      options={options}
+      value={value}
+      onQueryChange={setQuery}
+      onSelect={onSelect}
+      {...props}
+    />
+  );
+}
