0ded6844ac8cf87b9ca7b03c952ce2107e3613e8
Jeff Sieu <jeffsy00@gmail.com>
2022-11-08 02:34:58 +0800

    [questions][fix] fix lists column resizing

--- apps/portal/src/pages/questions/lists.tsx
+++ apps/portal/src/pages/questions/lists.tsx
@@ -166,7 +166,7 @@ export default function ListPage() {
             {listOptions}
           </aside>
           <section className="flex min-h-0 flex-1 flex-col items-center overflow-auto">
-            <div className="flex min-h-0 max-w-3xl flex-1 p-4">
+            <div className="flex min-h-0 w-full max-w-3xl flex-1 p-4">
               <div className="flex flex-1 flex-col items-stretch justify-start gap-4">
                 <div className="flex items-end gap-2 lg:hidden">
                   <div className="flex-1">
