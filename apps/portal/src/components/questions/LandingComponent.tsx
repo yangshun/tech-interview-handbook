3b965cc17337e38fb8381e01098b2859ee5a7af7
Jeff Sieu <jeffsy00@gmail.com>
2022-11-09 11:27:19 +0800

    [questions][ui] fix landing dropdown being clipped

--- apps/portal/src/components/questions/LandingComponent.tsx
+++ apps/portal/src/components/questions/LandingComponent.tsx
@@ -83,7 +83,7 @@ export default function LandingComponent({ onLanded }: LandingComponentProps) {
               </div>
             </div>
             <div className="mt-16 sm:mt-24 lg:col-span-6 lg:mt-0">
-              <div className="bg-white shadow sm:mx-auto sm:w-full sm:max-w-md sm:overflow-hidden sm:rounded-lg">
+              <div className="bg-white shadow sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg">
                 <div className="px-4 py-8 sm:px-10">
                   <div>
                     <Button
