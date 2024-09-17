5c7069049dcd72368de663a7cd105b6342e8d27f
Yangshun <tay.yang.shun@gmail.com>
2024-09-17 20:06:14 +0800

    web: add function to redirect Grind 75 path

---
+++ apps/website/functions/grind75/[[catchall]].js
@@ -0,0 +1,13 @@
+const SOURCE_HOST = 'https://grind75.vercel.app';
+
+export async function onRequest(context) {
+  const { request } = context;
+  // Define the original and target paths
+  const url = new URL(request.url);
+
+  // Rewrite to the new domain, preserving the original path and query string
+  const newURL = SOURCE_HOST + url.pathname + url.search;
+
+  // Fetch the content from the new domain
+  return await fetch(newURL, request);
+}
