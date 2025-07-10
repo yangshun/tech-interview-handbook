c5b41471322481b513d150dce9ef0c0f8f8713f4
Zhou Yuhang <54398705+xquisite0@users.noreply.github.com>
2025-07-10 08:25:52 +0800

    seo: fix HTML lang attribute missing (#714)

--- apps/portal/src/pages/_document.tsx
+++ apps/portal/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html className="bg-slate-50">
+    <Html lang="en" className="bg-slate-50">
       <Head />
       <body>
         <Main />
